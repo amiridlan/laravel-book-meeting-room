5a505f2849690a552179965b8a470088c8a4a6fd
Amir Idlan <amiridlan1999@gmail.com>
2024-12-13 09:58:10 +0800

    remove old code

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\SessionController;
-use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 // Main page route
@@ -11,33 +10,12 @@ Route::get('/', [BookingController::class, 'index'])->name('home');
 Route::get('/meeting-rooms', [BookingController::class, 'showAll'])->name('meeting.rooms');
 Route::get('/rooms/meeting-room/{room}', [BookingController::class, 'show'])->name('meeting.room');
 
-
-
 // Route::resource('booking', BookingController::class);
 Route::get('/booking/{room}', [BookingController::class, 'create'])->name('booking.create');
 Route::post('/booking', [BookingController::class, 'store'])->name('booking.store');
 Route::get('/booking/edit/{id}', [BookingController::class, 'edit'])->name('booking.edit');
 Route::put('/booking/update/{id}', [BookingController::class, 'update'])->name('booking.update');
 Route::delete('/booking/delete/{id}', [BookingController::class, 'destroy'])->name('booking.delete');
-
-
-
-// Contact page route
-Route::view('/contact', 'contact');
-
-// Job routes
-Route::resource('jobs', JobController::class);
-
-// Route::controller(JobController::class)->group(function () {
-//     Route::get('/jobs', 'index');
-//     Route::get('/jobs/create', 'create');
-//     Route::get('/jobs/{job}', 'show');
-//     Route::post('/jobs', 'store');
-//     Route::get('/jobs/{job}/edit', 'edit');
-//     Route::patch('/jobs/{job}', 'update');
-//     Route::delete('/jobs/{job}', 'destroy');
-// });
-
 
 //                                              AUTHENTICATION
 //Register

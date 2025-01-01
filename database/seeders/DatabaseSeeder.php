fc13f254f549f32faae5187a73c81ef18e0be4a9
Amir Idlan <amiridlan1999@gmail.com>
2025-01-01 17:34:28 +0800

    repaired database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Amir',
+            'email' => 'amir@example.com',
         ]);
 
-        $this->call(JobSeeder::class);
+        $this->call([
+            DepartmentsTableSeeder::class,
+            RoomSeeder::class,
+        ]);
     }
 }
